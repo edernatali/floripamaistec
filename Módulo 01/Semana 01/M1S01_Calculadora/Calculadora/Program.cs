3aa916aa6b09d0270507a864f010f6d097a1a6f3
edernatali <edernatali@hotmail.com>
2023-03-05 23:52:59 -0300

    Meu primeiro commit

---
+++ Módulo 01/Semana 01/M1S01_Calculadora/Calculadora/Program.cs
@@ -0,0 +1,65 @@
+﻿// Importação de bibliotecas 
+
+// Nome da classe
+
+// Declaração de método
+
+// Declaração de variáveis
+int opcao, numero1=0, numero2=0;
+float resultado=0;
+int resto;
+
+// Entradas
+Console.WriteLine("Escolha uma operação: \n\n" +
+    "1 - soma\n2 - subtração\n3 - multiplicação\n4 - divisão\n\n" +
+    "5 - sair\n\nDigite a opção escolhida: ");
+
+opcao = int.Parse(Console.ReadLine());
+
+// Processamento
+switch(opcao)
+{
+    // Bloco de instruções da estrutura condicional
+    case 1:
+        Console.WriteLine("Operação somar.");
+        Console.WriteLine("Digite o numero 1: ");
+        numero1 = int.Parse(Console.ReadLine());    
+        Console.WriteLine("Digite o número 2: ");
+        numero2 = int.Parse(Console.ReadLine());
+        resultado = numero1 + numero2;
+        break;
+    case 2:
+        Console.WriteLine("Operação subtrair.");
+        Console.WriteLine("Digite o numero 1: ");
+        numero1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o número 2: ");
+        numero2 = int.Parse(Console.ReadLine());
+        resultado = numero1 - numero2;
+        break;
+    case 3:
+        Console.WriteLine("Operação multiplicação.");
+        Console.WriteLine("Digite o numero 1: ");
+        numero1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o número 2: ");
+        numero2 = int.Parse(Console.ReadLine());
+        resultado = numero1 * numero2;
+        break;
+    case 4:
+        Console.WriteLine("Operação divisão.");
+        Console.WriteLine("Digite o numero 1: ");
+        numero1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o número 2: ");
+        numero2 = int.Parse(Console.ReadLine());
+        resultado = numero1 / numero2;
+        resto= numero1 % numero2;
+        Console.WriteLine("O resto da divisão é: " + resto);
+        break;
+    case 5:
+        Console.WriteLine("Saindo do sistema.");
+        break;
+
+}
+Console.WriteLine("O resultado é: " + resultado);
+
+
+// Saídas
---
+++ Módulo 01/Semana 01/M1S01_EX2_Lista_Frutas/Lista_Frutas/Program.cs
@@ -0,0 +1,25 @@
+﻿//[M1S01] Ex 2 - Organizador de lista de frutas
+//Crie um programa que receba do usuário uma lista de frutas com no máximo 15 itens.
+//Organize as frutas em ordem alfabética e exiba a lista.
+
+List<string> frutas = new List<string>();
+
+for (int i = 0; i < 15; i++)
+{   
+    Console.WriteLine("Digite o nome de uma fruta: ");
+    frutas.Add(Console.ReadLine());
+    Console.WriteLine("Deseja continuar? (S - sim; N - não)  ");
+    string resposta = Console.ReadLine();
+
+    if (resposta == "nao" || resposta == "n")
+    {
+        break;
+    }
+}
+
+frutas.Sort();
+
+foreach (string fruta in frutas)
+{
+    Console.WriteLine(fruta);
+}
