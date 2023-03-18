ff739a22cf2bea24b412ea76764c1aaac746db19
edernatali <edernatali@hotmail.com>
2023-03-17 23:54:42 -0300

    Enviando exercicio 1 da semana 3 do modulo 1

---
+++ Módulo 01/Semana 03/M1S03_Ex1_ClasseConstrutor/M1S03_Ex1_ClasseConstrutor/Program.cs
@@ -0,0 +1,8 @@
+﻿using M1S03_Ex1_ClasseConstrutor;
+
+Produto produto1 = new Produto("Calculadora", 5.75, 199);
+Produto produto2 = new Produto();
+
+Console.WriteLine($"O produto 1 tem nome: {produto1.nome}, valor: {produto1.preco}, e quantidade: {produto1.quantidade}");
+
+Console.WriteLine($"O produto 2 tem nome: {produto2.nome}, valor: {produto2.preco}, e quantidade: {produto2.quantidade}");
